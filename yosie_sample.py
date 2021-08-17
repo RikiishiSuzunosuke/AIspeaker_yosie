e8808eb6f62327d7d212ff0da4c900d77046b65e
RikiishiSuzunosuke <200221bt@yse-c.net>
2021-08-17 14:37:54 +0900

    yosie_sample.py update

--- yosie_sample.py
+++ yosie_sample.py
@@ -2,6 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 import sys
 import datetime
+import re
+import random
 
 def yosie_action(func):
 	if func == "天気":
@@ -22,7 +24,31 @@ def yosie_action(func):
 		print(phrase)
 		
 	elif func == "ニュース":
-		print("news")
+		URL = "https://www.yahoo.co.jp/"
+		rest = requests.get(URL)
+
+		soup = BeautifulSoup(rest.text, "html.parser")
+
+		data_list = soup.find_all(href=re.compile("news.yahoo.co.jp/pickup"))
+
+		headline_link_list = [data.attrs["href"] for data in data_list]
+
+		headline_link = headline_link_list[random.randrange(8)]
+
+		summary = requests.get(headline_link)
+		summary_soup = BeautifulSoup(summary.text, "html.parser")
+
+		summary_soup_a = summary_soup.select("a:contains('続きを読む')")[0]
+
+		news_body_link = summary_soup_a.attrs["href"]
+
+		news_body = requests.get(news_body_link)
+		news_soup = BeautifulSoup(news_body.text, "html.parser")
+		print(news_soup.title.text)
+
+		datail_text = news_soup.find(class_=re.compile("Direct"))
+
+		print(datail_text.text if hasattr(datail_text, "text") else '', end="\n\n\n")
 
 	elif func == "日時":
 		dt_now = datetime.datetime.now()
