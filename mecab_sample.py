2fbb27a5e5b055b1e9aeefcda9262fab55dc63ee
RikiishiSuzunosuke <200221bt@yse-c.net>
2021-10-15 14:15:53 +0900

    mecab_sample.py added

---
+++ mecab_sample.py
@@ -0,0 +1,17 @@
+import MeCab
+
+tagger = MeCab.Tagger("-Ochasen")
+str = input("input:")
+node = tagger.parseToNode(str)
+
+#形態素解析を実行して表示
+mecab = tagger.parse(str)
+print(mecab)
+
+#名詞　一般の単語のみを抽出して表示
+while node:
+    if node.surface != "":  # ヘッダとフッタを除外
+    	#ここを替えると、好きな品詞を抽出可能
+        if (node.feature.split(",")[0] == "名詞") and (node.feature.split(",")[1] == "一般"):
+            print(node.feature.split(",")[6])
+    node = node.next
