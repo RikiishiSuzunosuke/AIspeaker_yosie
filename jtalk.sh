cae6be5b2db45635b763f951689df92ae98a511c
RikiishiSuzunosuke <200221bt@yse-c.net>
2021-10-19 13:41:42 +0900

    ラズパイからシェルスクリプトを持ってきました

---
+++ jtalk.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+tempfile=`tempfile`
+option="-m /usr/share/hts-voice/mei/mei_happy.htsvoice \
+-x /var/lib/mecab/dic/open-jtalk/naist-jdic \
+-r 1.7 \
+-a 0.55 \
+-ow $tempfile result.txt"
+
+echo "$1" | open_jtalk $option
+aplay -q $tempfile
+rm $tempfile
