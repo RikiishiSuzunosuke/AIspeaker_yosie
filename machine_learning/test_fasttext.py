bdd41643ba621108e8aff080da3d6b2ebb8cdac4
superTonguman <84367300+superTonguman@users.noreply.github.com>
2021-10-26 14:20:48 +0900

    added option

--- machine_learning/test_fasttext.py
+++ machine_learning/test_fasttext.py
@@ -1,6 +1,7 @@
 import fasttext as ft
 
-model = ft.train_supervised('train_data.txt', label_prefix = '__label__', epoch = 1000, loss = 'hs')
+model = ft.train_supervised('train_data.txt', label_prefix = '__label__', dim = 200, epoch = 1000, loss = 'hs')
+#minCount = 2 登場回数が２回未満の単語を無視するオプション
 model.save_model('model.bin')
 
-print(model.predict('可愛い芳江'))
+print(model.predict('ニュース'))
