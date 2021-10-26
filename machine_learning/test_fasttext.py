652700240a118917a6ac8d2e029728d80d2011db
RikiishiSuzunosuke <200221bt@yse-c.net>
2021-10-26 10:54:29 +0900

    update

--- machine_learning/test_fasttext.py
+++ machine_learning/test_fasttext.py
@@ -1,6 +1,8 @@
 import fasttext as ft
 
-model = ft.train_supervised('train_data.txt', label_prefix = '__label__', epoch = 1000, loss = 'hs')
+model = ft.train_supervised('train_data.txt', label_prefix = '__label__', epoch = 100000, loss = 'softmax',lr=0.3,dim=200)
 model.save_model('model.bin')
 
-print(model.predict('可愛い芳江'))
+result = model.predict('今日の運勢を教えて。星座占いどう？')
+print(result)
+
