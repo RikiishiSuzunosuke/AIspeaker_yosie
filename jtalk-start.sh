ca8fc24bec7bd117ff08269140a488d590819446
RikiishiSuzunosuke <200221bt@yse-c.net>
2021-11-10 13:01:06 +0900

    update

--- jtalk-start.sh
+++ jtalk-start.sh
@@ -2,9 +2,9 @@
 tempfile=`tempfile`
 option="-m /usr/share/hts-voice/mei/mei_happy.htsvoice \
 -x /var/lib/mecab/dic/open-jtalk/naist-jdic \
--r 1.5 \
+-r 1.3 \
 -a 0.55 \
--ow $tempfile result.txt"
+-ow $tempfile"
 
 echo "$1" | open_jtalk $option
 aplay -q $tempfile
