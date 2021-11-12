514c28d80f310bac160bf514cb2bfd91b0900c4a
superTonguman <84367300+superTonguman@users.noreply.github.com>
2021-11-12 09:27:26 +0900

    update

--- machine_learning/test_fasttext.py
+++ machine_learning/test_fasttext.py
@@ -4,10 +4,11 @@ import fasttext as ft
 model = ft.train_supervised('train_data.txt', label_prefix = '__label__',  dim = 200,  epoch = 9000, loss = 'hs')
 #minCount = 2 登場回数が２回未満の単語を無視するオプション
 model.save_model('model.bin')
+#model.binを生成
 
-print(model.predict('今日の天気は'))
+print(model.predict('ニュースを読んで'))
 
-results = model.test('test_data_label_0.txt')
+results = model.test('train_data.txt')
 print(results)
 #テストデータとlabelの一致の割合が見える
 
