e735868609650999e28668b70a8eeecb90a3168e
RikiishiSuzunosuke <200221bt@yse-c.net>
2021-11-22 12:14:08 +0900

    update

--- machine_learning/test_fasttext.py
+++ machine_learning/test_fasttext.py
@@ -1,12 +1,12 @@
 import fasttext as ft
 
 
-model = ft.train_supervised('train_data2.txt', label_prefix = '__label__',  dim = 200,  epoch = 153000, loss = 'hs')
+model = ft.train_supervised('train_data2.txt', label_prefix = '__label__',  dim = 100,  epoch = 153000, loss = 'hs')
 #minCount = 2 登場回数が２回未満の単語を無視するオプション
 model.save_model('model.bin')
 #model.binを生成
 
-print(model.predict('今日の天気は晴れかな'))
+print(model.predict('今日の占いの結果'))
 
 results = model.test('train_data.txt')
 print(results)
