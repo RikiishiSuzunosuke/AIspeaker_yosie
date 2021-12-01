14c311c2ea339b0333075e2b08a989316d4f3396
superTonguman <84367300+superTonguman@users.noreply.github.com>
2021-12-01 10:04:05 +0900

    update

---
+++ machine_learning/predict.py
@@ -0,0 +1,9 @@
+import fasttext as ft
+
+def predict(text):
+	model = ft.load_model('model.bin')
+	result = model.predict(text)
+	re = str(result[0])
+	index11 = re[11]
+	return index11
+print(predict("今朝の天気予報"))
