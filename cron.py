ecd5959c86f75471445ac1b9d1c6a2f2efe4ef05
RikiishiSuzunosuke <200221bt@yse-c.net>
2022-01-11 14:32:59 +0900

    update1/11

--- cron.py
+++ cron.py
@@ -2,25 +2,26 @@ import datetime
 import sc_News
 import sc_Weather
 import os
+import time
 def cron():
-	hour = 99
+	print("cron start")
+	dt_now = datetime.datetime.now()
+	hour = dt_now.hour
 	while True:
-		#現在時刻を取得
 		dt_now = datetime.datetime.now()
-		#hourを取得
+		print("running:"+str(dt_now))
 		dt_hour = dt_now.hour
 
-		#hourが更新された場合
 		if hour != dt_hour:
 			hour = dt_hour
-			#一時間毎にニュースを取得して更新する
-			sc_News　#ニュースをスクレイピング
-			os.system('./news_make.sh')　#音声ファイル作成
+			sc_News
+			os.system('./news_make.sh')
 			print("news ok")
 
-			#天気予報は2時、5時、8時、11時、14時、15時、17時、20時に発表
 			if hour in [2,5,8,11,14,15,17,20]:
-				sc_Weather　#天気予報をスクレイピング
+				sc_Weather
 				os.system('./weather-m.sh')
 				print("weather ok")
+
+		time.sleep(300)
 cron()
