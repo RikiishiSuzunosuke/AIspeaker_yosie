80c5f591d202d7fa7d1ee75a621e1907bed406e2
superTonguman <84367300+superTonguman@users.noreply.github.com>
2022-01-11 14:34:45 +0900

    add file

--- machine_learning/test_fasttext.py
+++ machine_learning/test_fasttext.py
@@ -2,15 +2,15 @@ import fasttext as ft
 
 
 
-model = ft.train_supervised('train_J_nw.txt', label_prefix = '__label__',  dim = 200,  epoch = 5000, loss = 'hs')
+model = ft.train_supervised('train_J_delete_word.txt', label_prefix = '__label__',  dim = 200,  epoch = 10000, loss = 'hs')
 
 #minCount = 2 登場回数が２回未満の単語を無視するオプション
 model.save_model('model.bin')
 #model.binを生成
 
-print(model.predict('今日は何曜日'))
+print(model.predict('星座'))
 
-results = model.test('train_data.txt')
-print(results)
+#results = model.test('train_data.txt')
+#print(results)
 #テストデータとlabelの一致の割合が見える
 
