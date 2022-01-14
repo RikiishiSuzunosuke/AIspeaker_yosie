959e84c94ba4dd7b63adc142ae2dfb90fc48cfc1
superTonguman <84367300+superTonguman@users.noreply.github.com>
2022-01-14 10:18:26 +0900

    update

--- machine_learning/test_fasttext.py
+++ machine_learning/test_fasttext.py
@@ -2,10 +2,10 @@ import fasttext as ft
 
 
 
-model = ft.train_supervised('train_J_delete_word.txt', label_prefix = '__label__',  dim = 200,  epoch = 10000, loss = 'hs')
+model = ft.train_supervised('Jonny.txt', label_prefix = '__label__',  dim = 200,  epoch = 10000, loss = 'hs')
 
 #minCount = 2 登場回数が２回未満の単語を無視するオプション
-model.save_model('model.bin')
+model.save_model('Jonny_model.bin')
 #model.binを生成
 
 print(model.predict('星座'))
