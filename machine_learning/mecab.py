9434fa90d113021b5df6628a8d145e063f9bf93d
RikiishiSuzunosuke <200221bt@yse-c.net>
2022-01-17 09:51:58 +0900

    machin_learning update

--- machine_learning/mecab.py
+++ machine_learning/mecab.py
@@ -1,12 +1,16 @@
-import fasttext as ft
 import MeCab
 
-def predict(text):
-	tagger = MeCab.Tagger("-Owakati")
-	node = tagger.parseToNode(text)
+tagger = MeCab.Tagger("-Owakati")
+while True:
+	str = input("input:")
+	node = tagger.parseToNode(str)
+
+	#形態素解析を実行して表示
+	# mecab = tagger.parse(str)
+	# print(mecab)
 
 	present = ""
-
+	#名詞　一般の単語のみを抽出して表示
 	while node:
 	    if node.surface != "":  # ヘッダとフッタを除外
 	    	#ここを替えると、好きな品詞を抽出可能
@@ -14,18 +18,8 @@ def predict(text):
 	            present = present + node.feature.split(",")[6] + " "
 	        elif (node.feature.split(",")[0] == "名詞") and (node.feature.split(",")[1] == "接尾"):
 	            present = present + node.feature.split(",")[6] + " "
-	        elif "時間" in text:
+	        elif "時間" in str:
 	        	present = present + "時間 "
-	        	text = text.replace("時間","")
+	        	str = str.replace("時間","")
 	    node = node.next
 	print(present)
-
-	model = ft.load_model('Jonny_model.bin')
-	result = model.predict(text)
-	re = str(result[0])
-	index11 = re[11]
-	return index11
-	print(index11)
-
-text = input("input:")
-predict(text)
