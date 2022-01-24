77024ee6d8cdbea8f7a8edfdd04539c33817fb0e
RikiishiSuzunosuke <200221bt@yse-c.net>
2022-01-24 13:50:22 +0900

    ppt update

--- machine_learning/test_fasttext.py
+++ machine_learning/test_fasttext.py
@@ -1,15 +1,11 @@
 import fasttext as ft
 
-
-
 model = ft.train_supervised('Jonny.txt', label_prefix = '__label__',  dim = 200,  epoch = 10000, loss = 'hs')
 
-#minCount = 2 登場回数が２回未満の単語を無視するオプション
-model.save_model('Jonny_model.bin')
 #model.binを生成
+model.save_model('Jonny_model.bin')
 
 print(model.predict('射手 座'))
-
 #results = model.test('train_data.txt')
 #print(results)
 #テストデータとlabelの一致の割合が見える
