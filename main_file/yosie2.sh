29222b48010f101b5b56fe3e038949cb9ad202ef
rikiishisuzunosuke <200221bt@yse-c.net>
2022-01-28 11:48:23 +0900

    update

---
+++ main_file/yosie2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+export LD_PRELOAD=/usr/lib/arm-linux-gnueabihf/libatomic.so.1.2.0
+cd ~/AIspeaker_yosie
+./jtalk-start.sh 起動中です。起動には二分ほどかかります。
+
+python3 sc_FT.py
+./fortune-m.sh
+python3 sc_FT2.py
+./ft2-m.sh
+python3 sc_Weather.py
+./weather-m.sh
+python3 sc_News.py
+./news_make.sh
+./muute_off.sh
+
+./jtalk-start.sh 起動しました。ご用件をお申し付けください。
+python3 function.py
